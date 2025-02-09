98a2b4679b8b547817429bad977df7baa9a35161
Abraham3105 <acascantebadilla31@gmail.com>
2025-02-09 13:26:15 -0600

    EA-08-Practicas-2 Abraham Cascante

---
+++ src/main/java/com/tarea/AbrahamTarea1Application.java
@@ -0,0 +1,13 @@
+package com.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrahamTarea1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbrahamTarea1Application.class, args);
+	}
+
+}
